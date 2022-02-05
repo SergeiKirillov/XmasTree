4bb4030491d6265b247c78219910a78d6b936bd8
serge <serge@HP-NOTE-HOME>
2022-02-05 19:05:08 +0600

    05.02.2021 +Add frmViewDB

--- FirTree/EditMessageDatabase.xaml.cs
+++ FirTree/EditMessageDatabase.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,39 @@ namespace FirTree
     /// <summary>
     /// Логика взаимодействия для EditMessageDatabase.xaml
     /// </summary>
+    /// 
+
+    public class Zadacha
+    {
+        public int id { get; set; }
+        public string dtTask { get; set;}
+        public string strTask { get; set;}
+        public string Categoria { get; set; }
+    }
+
+    public class ZadachContext : DbContext
+    {
+        public DbSet<Zadacha> Zadachas { get; set; }
+    }
+
     public partial class EditMessageDatabase : Window
     {
+       
         public EditMessageDatabase()
         {
             InitializeComponent();
+
+            this.Loaded += My_Loaded; 
+
+        }
+
+        private void My_Loaded(object sender, RoutedEventArgs e)
+        {
+            ZadachContext context = new ZadachContext();
+
+            //context.Zadachas.OrderBy(c => c.dtTask).Load();
+            context.Zadachas.Load();
+            this.dataGrid.ItemsSource = context.Zadachas.Local;
         }
     }
 }
